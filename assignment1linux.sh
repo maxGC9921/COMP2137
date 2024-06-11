d0156a82d1544f0aaea461fff305c19f2f639bf7
Maxime Thibodeau <200579921@student.georgianc.on.ca>
2024-06-11 15:22:33 -0400

    Added comments to beginning of scripts and also fixed gateway variable to only show address

--- assignment1linux.sh
+++ assignment1linux.sh
@@ -2,8 +2,10 @@
 userName=$USER
 dateTime="$(date)"
 hostnameReport="$(hostname)" 
+#This line will print only the name of the Operating System with the version
 operatingSystem="$(grep 'PRETTY_NAME' /etc/os-release | cut -d'"' -f2)"
-timeUp="$(uptime)"
+timeUp="$(uptime -p)"
+#This line will print info on the CPU, the delimiter will remove the "Model name" from the output while awk while be used to bring closer the output to "cpu:"
 cpuInfo="$(lscpu | grep 'Model name: ' | cut -d " " -f3- | awk '{$1=$1;print}')"
 cpuSpeed="$(grep 'cpu MHz' /proc/cpuinfo | cut -d ":" -f2 | uniq && sudo dmidecode -t processor | grep "Speed" | uniq | head -n 1 | awk '{$1=$1;print}')"
 ramInfo="$(hwinfo --memory | grep 'Memory Size' | cut -d ":" -f2)"
@@ -11,7 +13,7 @@ diskInfo="$(lshw -class disk | grep 'product' | cut -d ":" -f2 | awk '{$1=$1;pri
 videoCard="$(lshw -C display | grep vendor | cut -d ":" -f2 && lshw -C display | grep product | cut -d ":" -f2 )"
 fqdnInfo="$(sudo hostname -f)"
 hostAddress="$(hostname -I)"
-gatewayAddress="$(ip route | grep 'default')"
+gatewayAddress="$(ip route | grep 'default' |  cut -d " " -f3- | cut -d " " -f1)"
 dnsAddress="$(grep -i nameserver /etc/resolv.conf |head -1| awk '{print $2}')"
 interfaceName="$(hwinfo --network --short | cut -d " " -f3-)"
 ipAddresses="$(ip r)"
