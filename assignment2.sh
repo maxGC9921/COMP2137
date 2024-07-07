244f41b0fd33405dc21be6608322082019383b6b
Maxime Thibodeau <200579921@student.georgianc.on.ca>
2024-07-07 17:05:49 -0400

    Added a line of code for ip configuration along with an experimental code for ssh port 22

--- assignment2.sh
+++ assignment2.sh
@@ -9,6 +9,7 @@ ip addr show eth0 | grep -q '192.168.16.200/24'
 if [ $? -eq 0 ]; then
      sudo sed -i 's/192.168.16.200/192.168.16.21/' /etc/netplan/10-lxc.yaml
      sudo netplan apply
+     sudo sed -i 's/192.168.16.200/192.168.16.21/' /etc/hosts
 else
     echo "The address 192.168.16.200 has already been replaced"
 fi
@@ -31,3 +32,8 @@ if [ $? -eq 0 ]; then
 else
 	sudo apt install squid -y
 fi
+
+
+
+#sudo ufw allow proto tcp from 172.16.1.200 to any port 22
+
