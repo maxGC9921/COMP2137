eb4a06029c09d709e9c95acfa5be2f95a5ef2e91
Maxime Thibodeau <200579921@student.georgianc.on.ca>
2024-07-06 23:04:49 -0400

    SCript ip addres

---
+++ assignment2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat /etc/hosts
+
+
+apache2=$(dpkg-query -l | grep apache2)
+
+squid=$(dpkg-query -l | grep squid)
+
+ip addr show eth0 | grep -q '192.168.16.200/24'
+
+if [ $? -eq 0 ]; then
+     ifconfig eth0 down;
+     ifconfig eth0 192.168.16.200 255.255.255.0;
+     ifconfig eth0 up
+else
+    echo "Something went wrong"
+fi
