bec2a7fa75728839155d67d0a5de4c125a94f92a
Maxime Thibodeau <200579921@student.georgianc.on.ca>
2024-07-07 00:06:47 -0400

    Added lines of codes that will check if Apache2 and squid are installed

--- assignment2.sh
+++ assignment2.sh
@@ -3,16 +3,32 @@
 cat /etc/hosts
 echo " "
 
-apache2=$(dpkg-query -l | grep apache2)
-
-squid=$(dpkg-query -l | grep squid)
 
 ip addr show eth0 | grep -q '192.168.16.200/24'
 
+#if [ $? -eq 0 ]; then
+#     ifconfig eth0 down;
+#     ifconfig eth0 192.168.16.21 255.255.255.0;
+#     ifconfig eth0 up
+#else
+#    echo "Something went wrong"
+#fi
+
+
+
+
+dpkg-query -l | grep apache2
+
 if [ $? -eq 0 ]; then
-     ifconfig eth0 down;
-     ifconfig eth0 192.168.16.200 255.255.255.0;
-     ifconfig eth0 up
+	echo "Apache 2 is already installed"
 else
-    echo "Something went wrong"
+	apt install apache2
+fi
+
+dpkg-query -l | grep squid
+
+if [ $? -eq 0 ]; then
+	echo "Squid is already installed"
+else
+	apt install sqid
 fi
