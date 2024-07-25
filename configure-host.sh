40f19935c6967c5e41e7a1b58441a0a366884102
Maxime Thibodeau <200579921@student.georgianc.on.ca>
2024-07-25 10:38:35 -0400

    Started foundation of script

--- configure-host.sh
+++ configure-host.sh
@@ -2,46 +2,91 @@
 
 trap '' TERM HUP INT
 
-read -p "Enter the new hostname: " userHostname
+VERBOSE=false
+HOSTNAME="/etc/hostname"
+HOSTS="/etc/hosts"
+NETPLAN="/etc/netplan/10-lxc.yaml"
 
-if [ -z "$userHostname" ]; then
-    echo "No hostname entered. Exiting."
+logMessage() {
+    local message="$1"
+    if $VERBOSE; then
+        echo "$message"
+    fi
+    logger "$message"
+}
+
+updateHostname() {
+    local desiredName="$1"
+    local currentName=$(hostname)
+
+    if [ "$currentName" != "$desiredName" ]; then
+        echo "$desiredName" > "$HOSTNAME"
+        hostname "$desiredName"
+        logMessage "Hostname changed from $currentName to $desiredName"
+    else
+        if $VERBOSE; then
+            echo "Hostname is already set to $desiredName"
+        fi
+    fi
+}
+
+updatedIp() {
+    local interface="$1"
+    local ipAddress="$2"
+    if ! [[ "$ipAddress" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+        echo "Invalid IP address format: $ipAddress"
+        exit 1
+    fi
+
+    currentIp=$(ip addr show dev "$interface" | awk '/inet / {print $2}' | cut -d '/' -f 1)
+    if [ "$currentIp" == "$ipAddress" ]; then
+        logMessage "IP address of $interface is already $ipAddress. No change needed."
+    else
+        sudo ip addr flush dev "$interface"
+        sudo ip addr add "$ipAddress/24" dev "$interface"
+        sudo ip link set "$interface" up
+        logMessage "IP address of $interface changed from $currentIp to $ipAddress"
+   
+        sudo sed -i "s/\b$currentIp\b/$ipAddress/g" "$HOSTS"
+        logMessage "Updated IP address in $HOSTS from $currentIp to $ipAddress"
+   
+        sudo sed -i "s/\b$currentIp\b/$ipAddress/g" "$NETPLAN"
+        logMessage "Updated IP address in $NETPLAN from $currentIp to $ipAddress"
+
+        sudo netplan apply
+    fi
+}
+
+if [ "$#" -lt 4 ]; then
+    echo "Usage: $0 -name <hostname> -ip <ip_address> [-v]"
     exit 1
 fi
 
-sudo hostnamectl set-hostname "$userHostname"
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        -v)
+            VERBOSE=true
+            ;;
+        -name)
+            shift
+            desiredName="$1"
+            ;;
+        -ip)
+            shift
+            ipAddress="$1"
+            ;;
+        *)
+            echo "Unknown parameter passed: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
 
-if [ $? -eq 0 ]; then
-    echo "Hostname changed to: $userHostname"
-else
-    echo "Failed to change hostname."
-fi
+updateHostname "$desiredName"
 
-read -p "Enter the new IP address: " newIp
-
-if [ -z "$newIp" ]; then
-    echo "No IP address entered. Exiting."
-    exit 1
-fi
-
-networkDevice=$(ip route get 1 | awk '{print $5}')
-
-#sudo ip addr replace "$newIp" dev "$networkDevice"
-
-#sudo sed -i 's/192.168.16.200/192.168.16.21/' /etc/netplan/10-lxc.yaml
-#sudo netplan apply
-#sudo sed -i 's/^192.168.16.200\s\+server1/192.168.16.21\tserver1/g' /etc/hosts
-
-sudo sed -i "s/\b[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\b/$newIp/" /etc/netplan/10-lxc.yaml
-sudo netplan apply
-sudo sed -i "s/^\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}\s\+server1/$newIp\tserver1/" /etc/hosts
+updatedIp "eth0" "$ipAddress"
 
 
 
-
-if [ $? -eq 0 ]; then
-    echo "IP address changed to: $newIp"
-else
-    echo "Failed to change IP address."
-fi
 
