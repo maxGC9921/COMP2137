7ef5e4efc1ae25b6dccf9132a1dc0f7e17bc1000
Maxime Thibodeau <200579921@student.georgianc.on.ca>
2024-07-25 21:09:11 -0400

    Added hostentry function and adapted script to support that function.

--- configure-host.sh
+++ configure-host.sh
@@ -23,8 +23,6 @@ updateHostname() {
         echo "$desiredName" > "$HOSTNAME"
         hostname "$desiredName"
         logMessage "Hostname changed from $currentName to $desiredName"
-        
-        # Update /etc/hosts with the new hostname
         sudo sed -i "s/$currentName/$desiredName/g" "$HOSTS"
         logMessage "Updated hostname in $HOSTS from $currentName to $desiredName"
     else
@@ -34,17 +32,16 @@ updateHostname() {
     fi
 }
 
-
-
-updatedIp() {
+updateIp() {
     local interface="$1"
     local ipAddress="$2"
+
     if ! [[ "$ipAddress" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
         echo "Invalid IP address format: $ipAddress"
         exit 1
     fi
 
-    currentIp=$(ip addr show dev "$interface" | awk '/inet / {print $2}' | cut -d '/' -f 1)
+    local currentIp=$(ip addr show dev "$interface" | awk '/inet / {print $2}' | cut -d '/' -f 1)
     if [ "$currentIp" == "$ipAddress" ]; then
         logMessage "IP address of $interface is already $ipAddress. No change needed."
     else
@@ -52,10 +49,10 @@ updatedIp() {
         sudo ip addr add "$ipAddress/24" dev "$interface"
         sudo ip link set "$interface" up
         logMessage "IP address of $interface changed from $currentIp to $ipAddress"
-   
+        
         sudo sed -i "s/\b$currentIp\b/$ipAddress/g" "$HOSTS"
         logMessage "Updated IP address in $HOSTS from $currentIp to $ipAddress"
-   
+        
         sudo sed -i "s/\b$currentIp\b/$ipAddress/g" "$NETPLAN"
         logMessage "Updated IP address in $NETPLAN from $currentIp to $ipAddress"
 
@@ -63,36 +60,72 @@ updatedIp() {
     fi
 }
 
-if [ "$#" -lt 4 ]; then
-    echo "Usage: $0 -name <hostname> -ip <ip_address> [-v]"
-    exit 1
-fi
+updateHostEntry() {
+    local entryName="$1"
+    local entryIp="$2"
 
-while [ "$#" -gt 0 ]; do
-    case $1 in
-        -v)
-            VERBOSE=true
-            ;;
-        -name)
-            shift
-            desiredName="$1"
-            ;;
-        -ip)
-            shift
-            ipAddress="$1"
-            ;;
-        *)
-            echo "Unknown parameter passed: $1"
-            exit 1
-            ;;
-    esac
-    shift
-done
+    if ! grep -q "$entryName" "$HOSTS"; then
+        echo "$entryIp $entryName" | sudo tee -a "$HOSTS" > /dev/null
+        logMessage "Added host entry: $entryIp $entryName"
+    else
+        sudo sed -i "s/\b[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\b/$entryIp/" "$HOSTS"
+        logMessage "Updated host entry: $entryIp $entryName"
+    fi
+}
 
-updateHostname "$desiredName"
+parsedArguments() {
+    local desiredName=""
+    local ipAddress=""
+    local hostentryName=""
+    local hostentryIp=""
 
-updatedIp "eth0" "$ipAddress"
+    echo "Arguments received: $@"  
+    if [ "$#" -lt 4 ]; then
+        echo "Usage: $0 -name <hostname> -ip <ip_address> [-v] [-hostentry <name> <ip>]"
+        exit 1
+    fi
 
+    while [ "$#" -gt 0 ]; do
+        case $1 in
+            -v)
+                VERBOSE=true
+                ;;
+            -name)
+                shift
+                desiredName="$1"
+                ;;
+            -ip)
+                shift
+                ipAddress="$1"
+                ;;
+            -hostentry)
+                shift
+                hostentryName="$1"
+                shift
+                hostentryIp="$1"
+                ;;
+            *)
+                echo "Unknown parameter passed: $1"
+                exit 1
+                ;;
+        esac
+        shift
+    done
 
+    echo "Parsed values - Name: $desiredName, IP: $ipAddress, Host Entry Name: $hostentryName, Host Entry IP: $hostentryIp"  
 
+    if [ -z "$desiredName" ] || [ -z "$ipAddress" ]; then
+        echo "Both -name and -ip options must be provided."
+        exit 1
+    fi
+
+    updateHostname "$desiredName"
+    updateIp "eth0" "$ipAddress"
+
+    if [ -n "$hostentryName" ] && [ -n "$hostentryIp" ]; then
+        updateHostEntry "$hostentryName" "$hostentryIp"
+    fi
+}
+
+parsedArguments "$@"
 
