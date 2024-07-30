a94e84f18421ea78aafb74a598af40c8f1699ade
Maxime Thibodeau <200579921@student.georgianc.on.ca>
2024-07-30 15:39:05 -0400

    Added template

---
+++ lab3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+﻿
+scp configure-host.sh remoteadmin@server1-mgmt:/root
+ssh remoteadmin@server1-mgmt -- /root/configure-host.sh -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4
+﻿scp configure-host.sh remoteadmin@server2-mgmt:/root
+ssh remoteadmin@server2-mgmt -- /root/configure-host.sh -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3
+./configure-host.sh -hostentry loghost 192.168.16.3
+./configure-host.sh -hostentry webhost 192.168.16.4
