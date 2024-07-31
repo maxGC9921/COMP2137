a2225d546af564d6dab1264de004daf636f6e7e4
Maxime Thibodeau <200579921@student.georgianc.on.ca>
2024-07-31 12:49:36 -0400

    Experimented with funtions and commands

--- lab3.sh
+++ lab3.sh
@@ -1,9 +1,53 @@
 #!/bin/bash
 
-﻿
-scp configure-host.sh remoteadmin@server1-mgmt:/root
-ssh remoteadmin@server1-mgmt -- /root/configure-host.sh -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4
-﻿scp configure-host.sh remoteadmin@server2-mgmt:/root
-ssh remoteadmin@server2-mgmt -- /root/configure-host.sh -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3
-./configure-host.sh -hostentry loghost 192.168.16.3
-./configure-host.sh -hostentry webhost 192.168.16.4
+VERBOSE=false
+
+logMessage() {
+    local message="$1"
+    if $VERBOSE; then
+        echo "$message"
+    fi
+    logger "$message"
+}
+
+errorExit() {
+    local message="$1"
+    echo "$message" >&2
+    exit 1
+}
+
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        -v)
+            VERBOSE=true
+            ;;
+        *)
+            echo "Unknown option or argument: $1"
+            echo "Usage: $0 [-v]"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+transferExecution() {
+    local server="$1"
+    local arguments="$2"
+
+    logMessage "Transferring configure-host.sh to $server"
+    scp configure-host.sh remoteadmin@"$server":/root || errorExit "Failed to transfer script to $server"
+
+    logMessage "Executing configure-host.sh on $server"
+    ssh remoteadmin@"$server" "bash /root/configure-host.sh $arguments $( [ "$VERBOSE" = true ] && echo '-v' )" || errorExit "Failed to execute script on $server"
+}
+
+transferExecution "server1-mgmt" "-name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4"
+
+transferExecution "server2-mgmt" "-name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3"
+
+logMessage "Updating local /etc/hosts file"
+./configure-host.sh -hostentry loghost 192.168.16.3 $( [ "$VERBOSE" = true ] && echo '-v' )
+./configure-host.sh -hostentry webhost 192.168.16.4 $( [ "$VERBOSE" = true ] && echo '-v' )
+
+logMessage "Script execution completed"
+
